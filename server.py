0a46eb3dc5fa476e1b89be6d5f9127071eb10a06
Amara Im <amaraim@icloud.com>
2023-01-13 03:22:22 -0500

    next: download output, UI design, deploy

--- server.py
+++ server.py
@@ -1,32 +1,43 @@
 import os
 import zipfile
-from flask import Flask, request, redirect, url_for, flash, render_template
+from flask import Flask, request, redirect, send_file, flash, render_template
 from werkzeug.utils import secure_filename
+from multiprocessing import Process
+import pandas as pd
+import io
 
-# UPLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = "./data/input-files/"
+OUTPUT_FOLDER = "./data/output-file/"
 ALLOWED_EXTENSIONS = set(['zip'])
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    inputFiles = getInputFiles()
+    outputFile = getOutputFiles()
+    return render_template('index.html', inputFiles=inputFiles, outputFile=outputFile)
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def getInputFiles():
+    inputFiles = [f for f in os.listdir(UPLOAD_FOLDER) if os.path.isfile(os.path.join(UPLOAD_FOLDER, f))]
+    return inputFiles
+
+def getOutputFiles():
+    outputFile = [f for f in os.listdir(OUTPUT_FOLDER) if os.path.isfile(os.path.join(OUTPUT_FOLDER, f))]
+    return outputFile
+
+@app.route('/uploadZipfile', methods=['GET', 'POST'])
+def upload_zipfile():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'zipfile' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
+        file = request.files['zipfile']
+
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -37,8 +48,147 @@ def upload_file():
             zip_ref.extractall(UPLOAD_FOLDER)
             zip_ref.close()
             os.remove(os.path.join(UPLOAD_FOLDER, filename))
-            inputFiles = [f for f in os.listdir(UPLOAD_FOLDER) if os.path.isfile(os.path.join(UPLOAD_FOLDER, f))]
+            inputFiles = getInputFiles()
+        
     return render_template('index.html', inputFiles=inputFiles)
+
+def create_outputFile(checkFiles):
+    fixedColumns = ['Email', 'First Name', 'Last Name', 'Year', 'Number of Events']
+    output_file_path = os.path.join(OUTPUT_FOLDER, "output.xlsx")
+    if os.path.isfile(output_file_path):
+        output_df = pd.read_excel(output_file_path)
+    else:
+        output_df = pd.DataFrame(columns=fixedColumns)
+
+    for fileIdx in range(len(checkFiles)):
+        filename = checkFiles[fileIdx]
+        input_file_path = os.path.join(UPLOAD_FOLDER, filename)
+        df_input = pd.read_excel(input_file_path)
+
+        for index, input_row in df_input.iterrows():
+            output_row = {'Email': '', 'First Name': '', 'Last Name': '', 'Year': '', 'Number of Events': ''}
+
+            if input_row['Email'] in output_df['Email'].values:
+                old_output_row = output_df.loc[output_df['Email'] == input_row['Email']]
+                output_df = output_df[output_df['Email'] != input_row['Email']]
+
+                for col in old_output_row.keys():
+                    if col not in input_row.keys() or input_row[col] == '':
+                        if old_output_row[col].values[0] == '':
+                            output_row[col] = ''
+                        else:
+                            output_row[col] = old_output_row[col].values[0]
+                    else:
+                        if old_output_row[col].values[0] == '':
+                            output_row[col] = input_row[col]
+                        else:
+                            output_row[col] = old_output_row[col].values[0]
+
+            else:     
+                if input_row['Email'] != '':
+                    for col in fixedColumns:
+                        if col not in input_row.keys():
+                            output_row[col] = ''
+                        else: 
+                            output_row[col] = input_row[col]
+
+            output_row[filename] = 1   
+            output_df = pd.concat([output_df, pd.DataFrame([output_row])], ignore_index = True)
+
+        os.remove(input_file_path)
+    
+    output_df.fillna(0, inplace=True)
+    eventColumns = [c for c in output_df.columns if c not in fixedColumns]
+    output_df['Number of Events'] = output_df[eventColumns].sum(axis=1)
+    output_df = output_df.sort_values(by=['Number of Events'], ascending=False)
+
+    output_df.to_excel(output_file_path, sheet_name="All Attendance", index=False) 
+    return
+
+def filter_outputFile():
+    output_file_path = os.path.join(OUTPUT_FOLDER, "output.xlsx")
+    output_df = pd.read_excel(output_file_path)
+
+    output_df.fillna(0, inplace=True) # fill empty cells with 0s
+    output_df = output_df[output_df['Email'] != 0] # remove cells with 0 value as 'Email'
+
+    # remove empty spaces and turn all strings to lowercase
+    for col in output_df.columns: 
+        output_df[col] = output_df[col].apply(lambda s: s.lower() or s.replace(" ", "") if type(s) == str else s)
+
+    fixedColumns = ['Email', 'First Name', 'Last Name', 'Year']
+
+    # get a list of emails that are duplicated
+    dupEmailList = output_df['Email'].loc[output_df['Email'].duplicated()].tolist()
+
+    # combine values of events where emails are duplicates
+    for i in range(len(dupEmailList)):
+        dupEmailRows = output_df[output_df['Email'] == dupEmailList[i]]
+        output_df = output_df[output_df['Email'] != dupEmailList[i]]
+
+        new_email_row = {}
+        for col in dupEmailRows.head(1).columns:
+            if col in fixedColumns:
+                new_email_row[col] = dupEmailRows.head(1)[col].values[0]
+            else:
+                new_email_row[col] = dupEmailRows[col].sum()            
+        output_df = pd.concat([output_df, pd.DataFrame([new_email_row])], ignore_index = True)
+
+    # get a list of names that are duplicated
+    dupNameList = output_df.loc[output_df['Last Name'].duplicated(keep=False)] 
+    dupFirstNameList = dupNameList['First Name'].loc[dupNameList['First Name'].duplicated()].tolist() 
+    dupLastNameList = dupNameList['Last Name'].loc[dupNameList['First Name'].duplicated()].tolist() 
+
+    # combine values of events where emails are duplicates
+    for i in range(len(dupFirstNameList)):
+        dupNameRows = output_df[(output_df['First Name'] == dupFirstNameList[i]) & (output_df['Last Name'] == dupLastNameList[i])]
+        output_df = output_df[~((output_df['First Name'] == dupFirstNameList[i]) & (output_df['Last Name'] == dupLastNameList[i]))]
+        sbuemail = dupNameRows.loc[dupNameRows['Email'].str.contains('@stonybrook.edu')]
+
+        new_name_row = {}
+        for col in sbuemail.columns:
+            if col in fixedColumns:
+                new_name_row[col] = sbuemail[col].values[0]
+            else:
+                new_name_row[col] = dupNameRows[col].sum()               
+        output_df = pd.concat([output_df, pd.DataFrame([new_name_row])], ignore_index = True)
+
+    output_df = output_df.sort_values(by=['Number of Events'], ascending=False)   
+    output_df.to_excel(output_file_path, sheet_name="All Attendance", index=False) 
+    return
+
+@app.route('/checkedFiles', methods=['GET', 'POST'])
+def submit_checkedFiles():
+    if request.method == 'POST':
+        checkFiles = request.form.getlist('checkedFiles')
+        inputFiles = getInputFiles()
+        inputFiles = [f for f in inputFiles if f not in checkFiles]
+        create_outputFile(checkFiles)
+        filter_outputFile()
+        outputFile = getOutputFiles()
+    return render_template('index.html', inputFiles=inputFiles, outputFile=outputFile)
+
+@app.route('/downloadOutput', methods=['GET', 'POST'])
+def download_file():
+    if request.method == 'POST':
+        file_path = os.path.join(OUTPUT_FOLDER, "output.xlsx")
+
+        return_data = io.BytesIO()
+        with open(file_path, 'rb') as fo:
+            return_data.write(fo.read())
+            return_data.seek(0)    
+
+        background_remove(file_path)
+
+        return send_file(return_data, as_attachment=True, mimetype='application/vnd.ms-excel', download_name='output.xlsx')
+    return render_template('index.html', inputFiles=getInputFiles(), outputFile=getOutputFiles())
+
+def background_remove(path):
+    task = Process(target=rm(path))
+    task.start()
+
+def rm(path):
+    os.remove(path)
 
 if __name__ == '__main__':
    app.run(debug = True)
